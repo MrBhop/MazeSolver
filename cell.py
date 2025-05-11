e1609ae870624c33ee26fec40a23960541938425
Florian Schuett <flo.schuett@chello.at>
2025-05-11 17:54:05 +0200

    Add function for drawing path from one cell to antother

--- cell.py
+++ cell.py
@@ -31,3 +31,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+            
+            
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+
+        self._win.draw_line(Line(self.center(), to_cell.center()), color)
+
+    
+    def center(self):
+        if self._x1 is None or self._y1 is None or self._x2 is None or self._y2 is None:
+            return
+
+        return Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from cell import *
 def main():
     win = Window(600, 800)
     cell = Cell(win)
+    cell.has_right_wall = False
     cell.draw(100, 100, 200, 200)
     cell_2 = Cell(win)
+    cell_2.has_left_wall = False
     cell_2.has_bottom_wall = False
-    cell_2.draw(200, 200, 300, 300)
+    cell_2.draw(200, 100, 300, 200)
+    cell.draw_move(cell_2, undo=True)
     win.wait_for_close()
 
 
