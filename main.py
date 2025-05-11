9eda0f0aefe9b6cea9b7b425ae191d1ec68c5008
Florian Schuett <flo.schuett@chello.at>
2025-05-11 17:41:25 +0200

    Add cell class

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from graphics import *
+from cell import *
 
 def main():
     win = Window(600, 800)
-    win.draw_line(Line(Point(0, 0), Point(300, 300)))
+    cell = Cell(win)
+    cell.draw(100, 100, 200, 200)
+    cell_2 = Cell(win)
+    cell_2.has_bottom_wall = False
+    cell_2.draw(200, 200, 300, 300)
     win.wait_for_close()
 
 
