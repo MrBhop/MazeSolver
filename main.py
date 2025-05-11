5fc8e39f210478172ff2a8185cc5ecbf54fe7f30
Florian Schuett <flo.schuett@chello.at>
2025-05-11 21:57:21 +0200

    Remove random seed

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from maze import *
 import sys
 
 def main():
-    sys.recursionlimit(10000)
+    sys.setrecursionlimit(10000)
     win = Window(600, 800)
-    maze = Maze(100, 100, 10, 10, 50, 50, win, 0)
+    maze = Maze(100, 100, 10, 10, 50, 50, win)
     print("solving maze ...")
     if maze.solve():
         print("maze solved!")
