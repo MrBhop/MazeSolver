862a7be54b52d77eca4ca7d790fb6da68e2c16fe
Florian Schuett <flo.schuett@chello.at>
2025-05-11 21:19:19 +0200

    Adjust wall breaking logic - took some ideas from https://www.boot.dev/solution/fc39e6d4-86b6-4c6c-8860-f9a552a85fc5

--- maze.py
+++ maze.py
@@ -52,59 +52,50 @@ class Maze():
         to_visit = []
 
         # check left
-        if i > 0:
-            if not cells[i - 1][j].visited:
-                to_visit.append(("l", (i - 1, j)))
+        if i > 0 and not cells[i - 1][j].visited:
+                to_visit.append((i - 1, j))
                 
         # check right
-        if i < len(cells) - 1:
-            if not cells[i + 1][j].visited:
-                to_visit.append(("r", (i + 1, j)))
+        if i < len(cells) - 1 and not cells[i + 1][j].visited:
+                to_visit.append((i + 1, j))
 
         # check top
-        if j > 0:
-            if not cells[i][j - 1].visited:
-                to_visit.append(("t", (i, j - 1)))
+        if j > 0 and not cells[i][j - 1].visited:
+                to_visit.append((i, j - 1))
         
         # check bottom
-        if j < len(cells[i]) - 1:
-            if not cells[i][j + 1].visited:
-                to_visit.append(("b", (i, j + 1)))
+        if j < len(cells[i]) - 1 and not cells[i][j + 1].visited:
+                to_visit.append((i, j + 1))
             
 
         while len(to_visit) > 0:
             target = to_visit.pop(random.randint(0, len(to_visit) - 1))
-            target_direction = target[0]
-            target_cell = target[1]
             
-            if cells[target_cell[0]][target_cell[1]].visited:
+            if cells[target[0]][target[1]].visited:
                 continue
             
-            self.break_walls_between_cells((i, j), target_direction, target[1])
-            self.break_walls(target_cell[0], target_cell[1])
+            self.break_walls_between_cells((i, j), target)
+            self.break_walls(target[0], target[1])
             
             
-    def break_walls_between_cells(self, cell1_tuple, direction, cell2_tuple):
+    def break_walls_between_cells(self, cell1_tuple, cell2_tuple):
         i1, j1 = cell1_tuple
         cell1 = self._cells[i1][j1]
         i2, j2 = cell2_tuple
         cell2 = self._cells[i2][j2]
 
-        match direction:
-            case "l":
-                cell1.has_left_wall = False
-                cell2.has_right_wall = False
-            case "r":
-                cell1.has_right_wall = False
-                cell2.has_left_wall = False
-            case "t":
-                cell1.has_top_wall = False
-                cell2.has_bottom_wall = False
-            case "b":
-                cell1.has_bottom_wall = False
-                cell2.has_top_wall = False
-            case _:
-                raise ValueError(f"'{direction}' is not a valid value.")
+        if i1 - 1 == i2:
+            cell1.has_left_wall = False
+            cell2.has_right_wall = False
+        if i1 + 1 == i2:
+            cell1.has_right_wall = False
+            cell2.has_left_wall = False
+        if j1 - 1 == j2:
+            cell1.has_top_wall = False
+            cell2.has_bottom_wall = False
+        if j1 + 1 == j2:
+            cell1.has_bottom_wall = False
+            cell2.has_top_wall = False
             
         self._draw_cell(i1, j1)
         self._draw_cell(i2, j2)
